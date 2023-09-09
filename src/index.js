9b59b6fe8a7da272b55e2afa2803b58a640fbcf7
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2023-09-09 10:18:15 +0300

    header, content section added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import './scss/styles.scss';
+
+const burgerSwitcher = (event) => {
+    const burger = event.target.closest('.header__burger');
+    const burgerInfo = burger.dataset.info;
+
+    switch (burgerInfo) {
+        case 'open':
+            burger.classList.add('opened');
+            burger.dataset.info = 'close';
+            headerSwitcher('open');
+            break;
+        case 'close':
+            burger.classList.remove('opened');
+            burger.dataset.info = 'open';
+            headerSwitcher('close');
+            break;
+    }
+}
+
+const headerSwitcher = (type) => {
+    const header = document.querySelector('.header');
+    const headerNavigation = document.querySelector('.header__nav');
+    const headerLogo = header.querySelector('.body__logo');
+
+    switch (type) {
+        case 'open':
+            headerLogo.style.display = 'none';
+            headerNavigation.style.display = 'flex';
+            break;
+        case 'close':
+            headerLogo.style.display = 'block';
+            headerNavigation.style.display = 'none';
+    }
+}
+
+document.querySelector('.header__burger').addEventListener('click', burgerSwitcher);
